9e91b1e76a31be6256f7c3706e3b02987de0f14b
HiimDucManh <20520630@gm.uit.edu.vn>
2022-11-03 16:27:21 +0700

    Add project files.

---
+++ FinalProject/View/LoginView.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
+using System.Windows.Shapes;
+
+namespace FinalProject.View
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var bc = new BrushConverter();
+            btnMinimize.Background = (Brush)bc.ConvertFrom("#7F8487");
+            //btnMinimize.Foreground = Brushes.Black;
+        }
+
+        private void btnMinimize_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btnMinimize.Background = Brushes.Transparent;
+            btnMinimize.Foreground = Brushes.White;
+        }
+
+        private void btnClose_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //Uri uri = new Uri("Images/CloseBtnRed.png", UriKind.Relative);
+            //StreamResourceInfo streamInfo = Application.GetResourceStream(uri);
+
+            //BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+            //var brush = new ImageBrush();
+            //brush.ImageSource = temp;
+
+            btnClose.Background = Brushes.Red;
+        }
+
+        private void btnClose_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //Uri uri = new Uri("Images/CloseBtn.png", UriKind.Relative);
+            //StreamResourceInfo streamInfo = Application.GetResourceStream(uri);
+
+            //BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+            //var brush = new ImageBrush();
+            //brush.ImageSource = temp;
+
+            btnClose.Background = Brushes.Transparent;
+        }
+    }
+}
