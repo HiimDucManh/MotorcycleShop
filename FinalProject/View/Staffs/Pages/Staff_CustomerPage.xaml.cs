997e72744a3423b816ea7a0c24150f61e34f0b39
HiimDucManh <20520630@gm.uit.edu.vn>
2023-02-06 01:29:37 +0700

    DM

---
+++ FinalProject/View/Staffs/Pages/Staff_CustomerPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalProject.View.Staffs.Pages
+{
+    /// <summary>
+    /// Interaction logic for Staff_CustomerPage.xaml
+    /// </summary>
+    public partial class Staff_CustomerPage : Page
+    {
+        public Staff_CustomerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FinalProject/View/Staffs/StaffDashboard.xaml.cs
@@ -0,0 +1,76 @@
+﻿using FinalProject.View.Staffs.Pages;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FinalProject.View.Staffs
+{
+    /// <summary>
+    /// Interaction logic for StaffDashboard.xaml
+    /// </summary>
+    public partial class StaffDashboard : Window
+    {
+        public StaffDashboard()
+        {
+            InitializeComponent();
+            staffPageNavigate.Navigate(new Dashboard());
+        }
+
+        //Cartesian
+        //public void Cartesian()
+        //{
+        //    SeriesCollection = new SeriesCollection
+        //    {
+        //        new LineSeries
+        //        {
+        //            Values=new ChartValues<double>{ 50, 120, 110, 160, 150, 180, 120 }
+        //        },
+        //    };
+        //    DataContext = this;
+        //}
+        //public SeriesCollection SeriesCollection { get; set; }
+
+        private void CartesianChart_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void minimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void shutdownBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void customerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            staffPageNavigate.Navigate(new Staff_CustomerPage());
+        }
+
+        private void dashboardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            staffPageNavigate.Navigate(new Dashboard());
+        }
+    }
+}
