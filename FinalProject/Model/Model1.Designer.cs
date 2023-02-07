0283603cb97bcd8d925bdd519ef99eba7af6c8a7
KaLord <Khoa4112002@gmail.com>
2023-02-07 13:14:22 +0700

    Sửa database

--- FinalProject/Model/Model1.Designer.cs
+++ FinalProject/Model/Model1.Designer.cs
@@ -1,6 +1,4 @@
-
-// T4 code generation is enabled for model 'D:\UIT\Study\Semester5\OOAD\Backup\MotorcycleShop\FinalProject\Model\Model1.edmx'. 
-
+﻿// T4 code generation is enabled for model 'E:\QLDA\MotorcycleShop\FinalProject\Model\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
